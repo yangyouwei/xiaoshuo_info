ebf370d054792644fd728de29ce89f15d79de9a9
dabai <yang_youwei81@163.com>
2019-06-21 17:39:47 +0800

    first

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+//正则rules
+var ruleBookname	[]string
+var	ruleBookname []string
+var ruleBookname []string
+var rulesBookmenu []string
+
+//book信息
+type bookinfo struct {
+	Bookname string
+	Author string
+	Size int
+	Booksummary string
+	Chapternumber int
+	Bookmenu []string
+}
+
+func main()  {
+
+}
+
+//func createdir()  {
+//
+//}
+//
+//func writeintofile(s string,p string)  {
+//
+//}
+
+func find()  {
+
+}
+
