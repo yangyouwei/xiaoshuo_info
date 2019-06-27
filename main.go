5b0650cc54071c262660f05f06d21a6ff1eb6c71
dabai <yang_youwei81@163.com>
2019-06-27 09:19:52 +0800

    Update main.go

--- main.go
+++ main.go
@@ -13,57 +13,54 @@ import (
 	"strings"
 	"sync"
 )
+
 //并发
 var concurrenc int
-var filenamech = make(chan string,10)
+var filenamech = make(chan string, 10)
 var bookstorepath = "/data/zip/books/"
-
 
 //book信息
 type Bookinfo struct {
-	Bookid	int64	`db:"id"`
-	Bookname string	`db:"bookname"`
-	Boookauthor string `db:"boookauthor"`
-	Bookcahtpernum	int `db:"bookcahtpernum"`
-	Bookcomment	string `db:"bookcomment"`
-	Bookstorepath string `db:"bookstorepath"`
+	Bookid          int64  `db:"id"`
+	Bookname        string `db:"bookname"`
+	Boookauthor     string `db:"boookauthor"`
+	Bookcahtpernum  int    `db:"bookcahtpernum"`
+	Bookcomment     string `db:"bookcomment"`
+	Bookstorepath   string `db:"bookstorepath"`
+	Sourcesfilename string `db:"sourcesfilename"`
 }
 
-
-func main()  {
-	pathname,err := filepath.Abs("/data/zip/unrarfull")
-	if err != nil  {
+func main() {
+	pathname, err := filepath.Abs("/data/zip/unrarfull")
+	if err != nil {
 		fmt.Println("path error")
 		return
 	}
 	concurrenc = 5
 	wg := sync.WaitGroup{} //控制主程序等待，以便goroutines运行完
-	wg.Add(concurrenc+1)
-	go func(wg *sync.WaitGroup,filenamech chan string) {
-		GetAllFile(pathname,filenamech)
-		close(filenamech)   //关闭通道，以便读取通道的程序知道通道已经关闭。
-		wg.Done()	//一定在函数的内部的最后一行运行。否则可能函数没有执行完毕。
-	}(&wg,filenamech)
-	for i := 0;i < concurrenc; i++  {
-		go func(wg *sync.WaitGroup,filenamech chan string) {
+	wg.Add(concurrenc + 1)
+	go func(wg *sync.WaitGroup, filenamech chan string) {
+		GetAllFile(pathname, filenamech)
+		close(filenamech) //关闭通道，以便读取通道的程序知道通道已经关闭。
+		wg.Done()         //一定在函数的内部的最后一行运行。否则可能函数没有执行完毕。
+	}(&wg, filenamech)
+	for i := 0; i < concurrenc; i++ {
+		go func(wg *sync.WaitGroup, filenamech chan string) {
 			for {
-				filename,isclose := <- filenamech
-				if !isclose {	//判断通道是否关闭，关闭则退出循环
+				filename, isclose := <-filenamech
+				if !isclose { //判断通道是否关闭，关闭则退出循环
 					break
 				}
-				a := filepath.Base(filename)
-				fmt.Println(a)
 				dosomewrork(filename)
 			}
 			wg.Done()
-		}(&wg,filenamech)
+		}(&wg, filenamech)
 	}
 	wg.Wait()
-	fmt.Println("finish")
 }
 
 //获取文件名
-func GetAllFile(pathname string,fn_ch chan string) {
+func GetAllFile(pathname string, fn_ch chan string) {
 	rd, err := ioutil.ReadDir(pathname)
 	if err != nil {
 		fmt.Println("read dir fail:", err)
@@ -71,7 +68,7 @@ func GetAllFile(pathname string,fn_ch chan string) {
 	for _, fi := range rd {
 		if fi.IsDir() {
 			fullDir := pathname + "/" + fi.Name()
-			GetAllFile(fullDir,fn_ch)
+			GetAllFile(fullDir, fn_ch)
 			if err != nil {
 				fmt.Println("read dir fail:", err)
 			}
@@ -83,16 +80,16 @@ func GetAllFile(pathname string,fn_ch chan string) {
 }
 
 //文档处理函数
-func dosomewrork(fp string)  {
+func dosomewrork(fp string) {
 	b := Bookinfo{}
 	b.getinfo(fp)
 	b.insert()
 }
 
-func (b *Bookinfo)getinfo(fp string)  {
+func (b *Bookinfo) getinfo(fp string) {
 	//bookname
-	bn := strings.Split(filepath.Base(fp),".")
-	bookname := bn[1]
+	bn := strings.Split(filepath.Base(fp), ".")
+	bookname := bn[2]
 	b.Bookname = bookname
 	//作者
 	b.Boookauthor = getbookauthor(fp)
@@ -100,23 +97,24 @@ func (b *Bookinfo)getinfo(fp string)  {
 	b.Bookcahtpernum = 0
 	//摘要
 	b.Bookcomment = ""
+	b.Sourcesfilename = fp
 }
 
 //book信息写入数据库
-func (b *Bookinfo)insert()  {
+func (b *Bookinfo) insert() {
 	db, err := sql.Open("mysql", "root:gaopeng@tcp(192.168.2.250:3306)/books")
 	check(err)
 
-	stmt, err := db.Prepare(`INSERT bookinfo ( bookname, boookauthor, bookcahtpernum,bookcomment,bookstorepath) VALUES (?,?,?,?,?)`)
+	stmt, err := db.Prepare(`INSERT bookinfo ( bookname, boookauthor, bookcahtpernum,bookcomment,bookstorepath,sourcesfilename) VALUES (?,?,?,?,?,?)`)
 	check(err)
 
-	res, err := stmt.Exec(b.Bookname,b.Boookauthor,b.Bookcahtpernum,b.Bookcomment,b.Bookstorepath)
+	res, err := stmt.Exec(b.Bookname, b.Boookauthor, b.Bookcahtpernum, b.Bookcomment, b.Bookstorepath, b.Sourcesfilename)
 	check(err)
 
-	id, err := res.LastInsertId()  //必须是自增id的才可以正确返回。
+	id, err := res.LastInsertId() //必须是自增id的才可以正确返回。
 	check(err)
-	idstr := fmt.Sprintf("%v",id)
-	err = os.Mkdir(bookstorepath + idstr, os.ModePerm)
+	idstr := fmt.Sprintf("%v", id)
+	err = os.Mkdir(bookstorepath+idstr, os.ModePerm)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -124,10 +122,10 @@ func (b *Bookinfo)insert()  {
 	stmt, err = db.Prepare("UPDATE bookinfo set bookstorepath=? WHERE id=?")
 	check(err)
 
-	res, err = stmt.Exec(bookstorepath + idstr,id)
+	res, err = stmt.Exec(bookstorepath+idstr, id)
 	check(err)
 
-	_, err = res.RowsAffected()  //返回值是1 修改成功，如果是零则是不成功（修改值和原来的值一样，mysql实际没有修改，或者没有这条记录）
+	_, err = res.RowsAffected() //返回值是1 修改成功，如果是零则是不成功（修改值和原来的值一样，mysql实际没有修改，或者没有这条记录）
 	check(err)
 
 	fmt.Println(idstr)
@@ -135,7 +133,7 @@ func (b *Bookinfo)insert()  {
 }
 
 func check(err error) {
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 		panic(err)
 	}
@@ -151,7 +149,7 @@ func getbookauthor(fp string) string {
 	defer fi.Close()
 
 	br := bufio.NewReader(fi)
-	for i:=0;i<20;i++ {		//读取前20行
+	for i := 0; i < 20; i++ {
 		a, _, c := br.ReadLine()
 		if c == io.EOF {
 			break
@@ -167,27 +165,24 @@ func getbookauthor(fp string) string {
 
 //正则表达式
 func getname(s string) string {
-	isok , err := regexp.Match("作者：",[]byte(s))
+	isok, err := regexp.Match("作者：", []byte(s))
 	if err != nil {
 		fmt.Println(err)
 	}
 	if isok {
 		reg := regexp.MustCompile(".*(作者：)(.*)")   //分组，第一个分组是全部匹配的结果，第二个是括号里的。
-		result := reg.FindAllStringSubmatch(s,-1)  //使用for循环然后取切片的下标，或者使用result1[0][1]直接取出
+		result := reg.FindAllStringSubmatch(s, -1) //使用for循环然后取切片的下标，或者使用result1[0][1]直接取出
 		a := result[0][2]
-		fmt.Println(a)
 		return a
 	}
-
-	isok , err = regexp.Match("著",[]byte(s))
+	isok, err = regexp.Match("著", []byte(s))
 	if err != nil {
 		fmt.Println(err)
 	}
 	if isok {
-		reg := regexp.MustCompile("(.*)(著)(\\s*)")   //分组，第一个分组是全部匹配的结果，第二个是括号里的。
-		result := reg.FindAllStringSubmatch(s,-1)  //使用for循环然后取切片的下标，或者使用result1[0][1]直接取出
-		a := result[0][0]
-		fmt.Println(a)
+		reg := regexp.MustCompile("(.*)(著)(\\s*)") //分组，第一个分组是全部匹配的结果，第二个是括号里的。
+		result := reg.FindAllStringSubmatch(s, -1) //使用for循环然后取切片的下标，或者使用result1[0][1]直接取出
+		a := result[0][1]
 		return a
 	}
 	return ""
